9130c4a8c777d8a970036a880e22976310d032d0
aziz9898 <38816018+aziz9898@users.noreply.github.com>
2019-05-19 13:45:52 +0300

    Initial
    Getting started

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by aziz on 19/05/2019.
+//  Copyright © 2019 Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
