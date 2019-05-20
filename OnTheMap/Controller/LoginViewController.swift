d3faa48a668a08b5b3c76924dc33f6fd760de184
Abdulaziz Alrabiah <38816018+aziz9898@users.noreply.github.com>
2019-05-20 04:19:54 +0300

    LoginUI

---
+++ OnTheMap/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by aziz on 19/05/2019.
+//  Copyright © 2019 Aziz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var ActivityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func SignUpPressed(_ sender: Any) {
+        
+    }
+    @IBAction func LogInPressed(_ sender: Any) {
+        
+    }
+    
+}
+
